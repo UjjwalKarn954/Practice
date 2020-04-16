ac42de53515bfabd8582f619b871314d993be4a2
Ujjwal Kumar Karn <ujjwalkarn954@gmail.com>
2020-04-16 13:42:11 +0530

    Create CommonelementInt.java
    Common element in two int array.

---
+++ CommonelementInt.java
@@ -0,0 +1,22 @@
+public class CommonElementInt{
+   public static void main(string args[]){
+    Scanner scan=new Scanner(System.in);
+    int n=scan.nextInt();
+    int arr[]=new int[n];
+    int arr1[]=new int[n];
+    For(int i=0;i<n;i++){
+    arr[i]=scan.nextInt();
+    }
+     For(int i=0;i<n;i++){
+    arr1[i]=scan.nextInt();
+    }  
+    For(int i=0;i<arr.length;i++){
+     For(int j=0;j<arr1.length;++){
+      if(arr[i]==arr[j];
+      System.out.println(arr[i];
+    }
+    }
+    }
+    }
+    
+    
