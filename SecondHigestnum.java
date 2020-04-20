a2a01e42f3cad26300d667a57e0a794736ee6097
Ujjwal Kumar Karn <ujjwalkarn954@gmail.com>
2020-04-20 13:08:06 +0530

    Update SecondHigestnum.java

--- SecondHigestnum.java
+++ SecondHigestnum.java
@@ -1,1 +1,13 @@
+public class SecondHigestnum{
+  public Static void main(String args[]){
+    Scanner scan=new Scanner (System.in);
+    int n=scan.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=scan.nextInt();
+    }
+    arr.sort();
+    System.out.println(arr[n-1]);
+  }
+}
 
